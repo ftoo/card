504e4bcb6a54e94bd106da456c79974319799c02
ilabafrica <ilabafrica@ilabafricas-iMac-7.local>
2016-07-01 13:24:05 +0300

    calculator commit

---
+++ ios calculator/ios calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios calculator
+//
+//  Created by ilabafrica on 7/1/16.
+//  Copyright (c) 2016 ILabAfrica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios calculator/ios calculatorTests/ios_calculatorTests.m
@@ -0,0 +1,40 @@
+//
+//  ios_calculatorTests.m
+//  ios calculatorTests
+//
+//  Created by ilabafrica on 7/1/16.
+//  Copyright (c) 2016 ILabAfrica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ios_calculatorTests : XCTestCase
+
+@end
+
+@implementation ios_calculatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
